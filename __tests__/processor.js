3746306535b9c551274eaf5f0bbb948deaa8907c
risan <risanbagja@gmail.com>
2019-01-10 23:23:08 +0100

    Add more test to processor module

--- __tests__/processor.js
+++ __tests__/processor.js
@@ -2,29 +2,51 @@
 /* eslint no-console: "off" */
 const CssProcessor = require("../src/css/css-processor");
 const Config = require("../src/config");
+const MarkdownProcessor = require("../src/markdown/markdown-processor");
 const Processor = require("../src/processor");
 const remove = require("../src/fs/remove");
+const Renderer = require("../src/renderer");
 const StaticFileProcessor = require("../src/static-file-processor");
 
 jest.mock("../src/fs/remove");
 
-test("it has config property", () => {
+test("it has config, renderer, css, markdown, and staticFile properties", () => {
   const processor = new Processor();
 
   expect(processor.config).toBeInstanceOf(Config);
-});
-
-test("it has staticFile and css property", () => {
-  const processor = new Processor();
-
-  expect(processor.staticFile).toBeInstanceOf(StaticFileProcessor);
+  expect(processor.renderer).toBeInstanceOf(Renderer);
   expect(processor.css).toBeInstanceOf(CssProcessor);
+  expect(processor.markdown).toBeInstanceOf(MarkdownProcessor);
+  expect(processor.staticFile).toBeInstanceOf(StaticFileProcessor);
 });
 
 test("it can receive config options parameter", () => {
   const processor = new Processor({ url: "http://foo.bar" });
 
   expect(processor.config.url).toBe("http://foo.bar");
+});
+
+test("it set renderer's minify option to true on production", () => {
+  const processor = new Processor({ env: "production" });
+
+  expect(processor.renderer.minify).toBe(true);
+});
+
+test("it adds config data to renderer", () => {
+  const processor = new Processor({ env: "production" });
+
+  expect(processor.renderer.getGlobal("config")).toBeInstanceOf(Config);
+});
+
+test("it adds formatDate filter to renderer", () => {
+  const processor = new Processor({ env: "production" });
+
+  const formatDate = processor.renderer.env.getFilter("formatDate");
+
+  expect(formatDate).toBeInstanceOf(Function);
+  expect(formatDate(new Date("2018-08-01T12:00:00+00:00"), "DD MMM YYYY")).toBe(
+    "01 Aug 2018"
+  );
 });
 
 test("it can generate the website", async () => {
@@ -34,12 +56,14 @@ test("it can generate the website", async () => {
 
   processor.cleanOutputDir = jest.fn();
   processor.processCss = jest.fn();
+  processor.processMarkdown = jest.fn();
   processor.copyStaticFiles = jest.fn();
 
   await processor.process();
 
   expect(processor.cleanOutputDir).toHaveBeenCalled();
   expect(processor.processCss).toHaveBeenCalled();
+  expect(processor.processMarkdown).toHaveBeenCalled();
   expect(processor.copyStaticFiles).toHaveBeenCalled();
 });
 
@@ -64,6 +88,25 @@ test("it can process css files", async () => {
 
   expect(processor.css.processAll).toHaveBeenCalled();
   expect(assets).toEqual({ "main.css": "main.123.css" });
+});
+
+test("it can process markdown files", async () => {
+  const processor = new Processor();
+
+  processor.markdown.processAll = jest.fn().mockReturnValue({
+    posts: ["post1", "post2"],
+    pages: ["page1"],
+    collections: { foo: ["bar"] }
+  });
+
+  const data = await processor.processMarkdown();
+
+  expect(processor.markdown.processAll).toHaveBeenCalled();
+  expect(data).toEqual({
+    posts: ["post1", "post2"],
+    pages: ["page1"],
+    collections: { foo: ["bar"] }
+  });
 });
 
 test("it can copy static files", async () => {
